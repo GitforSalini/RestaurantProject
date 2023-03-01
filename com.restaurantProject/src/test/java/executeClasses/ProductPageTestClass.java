70aaf9e516328b5a8ebe4fef5be2af7356e24f74
salin <salin@Thiruvonam>
2023-03-01 14:07:38 +0530

    Developer Commit

--- com.restaurantProject/src/test/java/executeClasses/ProductPageTestClass.java
+++ com.restaurantProject/src/test/java/executeClasses/ProductPageTestClass.java
@@ -3,63 +3,82 @@ package executeClasses;
 import java.io.IOException;
 
 import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 import elementRepository.HomePage;
 import elementRepository.LoginPage;
 import elementRepository.ProductPage;
-
+import extendReport.TestListener;
+@Listeners(TestListener.class)
 public class ProductPageTestClass extends BaseClass
 
 {
 	LoginPage lp;
 	ProductPage pp;
+	String uniqCode;
+	
+	
 	
   @Test(priority=1,groups = {"group1"})
   public void VerifyTheProductPageIsOpenedByClickingTheProductTab() throws IOException 
   {
+	 
+	 TestListener.getTestInstance().get().assignCategory("Smoke");
+	  
 	  lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
+	
+	  lp.login();
 	  
 	  pp.productTabClick();
 	  Boolean actual=pp.isProducttypeTextDisplayed();
 	  Assert.assertTrue(actual);
   }
   
-  @Test(priority=2,groups = {"add"})
+  @Test(enabled=false,priority=2,groups = {"add"})
   public void VerifyToAddaNewProduct() throws IOException, InterruptedException 
   
   {
+	  TestListener.getTestInstance().get().assignCategory("Smoke");
 	  lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
+	 
+	  lp.login();
+	  uniqCode=pp.addProduct();
 	  
-	  pp.productTabClick();
+	 /* pp.productTabClick();
 	  pp.clickOnAddProduct();
 	  pp.selectType();
-	  pp.enterCode(pp.readProductCode(2,1));
+	 
+	  pp.clearfield();
+	  uniqCode=pp.enterCode();
+	  System.out.println(uniqCode);
 	  pp.enterName(pp.readProductName(3,1));
 	  pp.selectCategory();
 	  pp.enterPrice(pp.readPrice(4, 1));
+	  pp.clickAddproductSubmit();*/
 	  
-	//pp.clickChooseFileToUploadImage(System.getProperty("user.dir")+"\\src\\main\\resources\\lime.jfif");
-	  pp.clickAddproductSubmit();
+	  pp.refreshPage();
+	  pp.enterSearchValue(uniqCode);
+	  String expected=uniqCode;
+	  String actual=pp.searchById();
+	  Assert.assertEquals(expected,actual);
+	  
 	  
   }
   
-  @Test(priority=3,groups = {"search"})
+  @Test(enabled=false,priority=3,groups = {"search"})
   public void  VerifySearchFunctionalityByUsingProductName() throws IOException
   {
+	  TestListener.getTestInstance().get().assignCategory("Sanity");
 	  lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
+	
+	  lp.login();
 	  
 	  pp.productTabClick();
 	  pp.enterSearchValue("Lemon");
@@ -69,58 +88,61 @@ public class ProductPageTestClass extends BaseClass
 	  Assert.assertEquals(expected,actual);
   }
   
-  @Test(priority=4,groups = {"search"})
+  @Test(enabled=false,priority=4,groups = {"search"})
   public void  VerifySearchFunctionalityByUsingProductCode() throws IOException
   {
+	  TestListener.getTestInstance().get().assignCategory("Sanity");
 	  lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
+	
+	  lp.login();
 	  
 	  pp.productTabClick();
-	  pp.enterSearchValue("L5550");
-	  String expected="L5550";
+	  pp.enterSearchValue(uniqCode);
+	  String expected=uniqCode;
 	  String actual=pp.searchById();
 	  Assert.assertEquals(expected,actual);
   }
-  @Test(priority=5,groups = {"edit"})
-  public void  VerifyEditFunctionality() throws IOException
+  @Test(enabled=false,priority=5,groups = {"edit"})
+  public void  VerifyEditFunctionality() throws IOException, InterruptedException
   {
-	  lp=new LoginPage(driver);
+	 TestListener.getTestInstance().get().assignCategory("Regression");
+	 lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
 	  
-	  pp.productTabClick();
-	  pp.enterSearchValue("L5550");
+	  lp.login();
+	  uniqCode=pp.addProduct();
+	  
+	//  pp.productTabClick();
+	  
+	  pp.refreshPage();
+	  pp.enterSearchValue(uniqCode);
 	  pp.editoptionClick();
 	  pp.clearTax();
 	  pp.enterTax("3");
 	  pp.clickAddproductSubmit();
-	  pp.enterSearchValue("L5550");
+	  pp.enterSearchValue(uniqCode);
 	  
 	  Boolean actual=pp.isTaxDisplayed();
 	  Assert.assertTrue(actual);
 	  
 	  
   }
-  @Test(priority=6,groups = {"delete"})
+  @Test(enabled=false,priority=6,groups = {"delete"})
   public void  VerifyDeleteFunctionality() throws IOException
   {
+	  TestListener.getTestInstance().get().assignCategory("Regression");
 	  lp=new LoginPage(driver);
 	  pp=new ProductPage(driver);
-	  lp.enterUserName(lp.readUserName(0, 1));
-	  lp.enterpassword(lp.readPassword(1, 1));
-	  lp.clickOnLoginButton();
+	
+	  lp.login();
 	  
 	  pp.productTabClick();
-	  pp.enterSearchValue("L5550");
+	  pp.enterSearchValue(uniqCode);
 	  pp.clickDelete();
 	  
 	  
-	  pp.enterSearchValue("L5550");
+	  pp.enterSearchValue(uniqCode);
 	  String expected="No matching records found";
 	  String actual=pp.confirmingDelete();
 	  Assert.assertEquals(expected,actual);
